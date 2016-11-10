0bb493063458fa192c591dd9ba9565505297fbb9
Micha Alt <mail@lean-stack.de>
2016-11-10 23:13:31 +0100

    Watching and injecting bower dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,21 @@ var plugins = require('gulp-load-plugins')();
 
 // NPM packages
 var browserSync = require('browser-sync');
+var wiredep = require('wiredep').stream;
 
 var tasks = {
   serve: serve,
   serveDist: serveDist,
+  styles: styles,
   inject: inject,
+  bootstrap: bootstrapFonts,
   watch: watch,
   build: build
 };
 
-gulp.task('inject', tasks.inject);
+gulp.task('fonts', tasks.bootstrap);
+gulp.task('styles', tasks.styles);
+gulp.task('inject', ['styles','fonts'], tasks.inject);
 gulp.task('build', ['inject'], tasks.build);
 gulp.task('watch', ['inject'], tasks.watch);
 gulp.task('serve', ['inject','watch'], tasks.serve);
@@ -52,13 +57,26 @@ function serveDist(done) {
 }
 
 function inject() {
-  gulp.src(conf.paths.src + '/index.html')
+  return gulp.src(conf.paths.src + '/index.html')
+    .pipe(wiredep())
     .pipe(gulp.dest(conf.paths.tmp))
     .pipe(browserSync.stream());
 }
 
+function styles() {
+  return gulp.src(conf.paths.src + '/styles/*.scss')
+    .pipe(wiredep())
+    .pipe(plugins.sass())
+    .pipe(gulp.dest(conf.paths.tmp + '/styles'));
+}
+
+function bootstrapFonts() {
+  return gulp.src('bower_components/bootstrap-sass/assets/fonts/**/*.*')
+    .pipe(gulp.dest(conf.paths.tmp + '/fonts'));
+}
+
 function watch(done) {
-  gulp.watch(conf.paths.src + '/index.html', ['inject']);
+  gulp.watch([conf.paths.src + '/index.html','bower.json'], ['inject']);
   done();
 }
 
